8ecaf1593915dfd187a7297613e91ebabcc432d9
winer7 <70219756+winer7@users.noreply.github.com>
2025-01-27 02:10:24 +0000

    Premier commit de mon projet Spring Boot

---
+++ src/main/java/erwin/ProjectErwin2025/ProjectErwin2025Application.java
@@ -0,0 +1,13 @@
+package erwin.ProjectErwin2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectErwin2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectErwin2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/erwin/ProjectErwin2025/ProjectErwin2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package erwin.ProjectErwin2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectErwin2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
