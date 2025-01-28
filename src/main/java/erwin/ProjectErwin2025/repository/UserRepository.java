a00635f548918cf75bbad1503dea14c8e2b022fe
winer7 <70219756+winer7@users.noreply.github.com>
2025-01-28 15:25:07 +0000

    ajout element user

--- src/main/java/erwin/ProjectErwin2025/repository/UserRepository.java
+++ src/main/java/erwin/ProjectErwin2025/repository/UserRepository.java
@@ -3,7 +3,7 @@ package erwin.ProjectErwin2025.repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository  {
+
 }
 
